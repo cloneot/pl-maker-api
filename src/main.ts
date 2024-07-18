1a39b2bc5079eafd869faa7b1b9d238d3561955b
cloneot <cloneot@gmail.com>
2024-07-19 06:13:24 +0900

    feat(cors): add cors whitelist

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://pldemo.netlify.app/'],
+  });
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('app.port');
